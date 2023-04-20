77bf020fceb6075f86a2ec76fcd6ae1b34864680
Bo <nguyendqduy@gmail.com>
2023-04-20 13:59:41 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,18 +1,30 @@
 require("dotenv").config();
+import request from "request";
 const MY_VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const getHomePage = (req, res) => {
-  return res.send("xin chao1");
+  return res.send("xin chao123");
 };
 const postWebhook = (req, res) => {
   let body = req.body;
 
   if (body.object === "page") {
     body.entry.forEach(function (entry) {
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
-      let sender_psid = webhook_event.sender_id;
-      console.log("Sender PSID:" + sender_psid);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
     });
 
     res.status(200).send("OK");
@@ -44,6 +56,46 @@ const getWebhook = (req, res) => {
     }
   }
 };
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an image!`,
+    };
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: MY_VERIFY_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}
 module.exports = {
   getHomePage: getHomePage,
   getWebhook: getWebhook,
